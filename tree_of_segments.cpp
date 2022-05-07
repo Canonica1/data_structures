245ad0ac9fb472b1447b27efe0a050fce1073004
Canonica1 <88035227+Canonica1@users.noreply.github.com>
2022-05-08 00:03:20 +0300

    Update tree_of_segments.cpp

--- tree_of_segments.cpp
+++ tree_of_segments.cpp
@@ -19,43 +19,60 @@ using namespace std;
 
 int N = 1001;
 void build (int v, int l, int r, vector<int> &t, vector<int> &a) {
-	if (r- l == 1)
+	if (r == l)
 	{
 		t[v] = a[l];
 		return;
 	}
 	int m = (l + r) / 2;
 	build(2 * v + 1, l, m, t, a); //левая граница
-	build(2 * v + 2, m, r, t, a); //правая граница
+	build(2 * v + 2, m + 1, r, t, a); //правая граница
 	t[v] = t[2 * v + 1]+ t[2 * v + 2];
 }
-int get_sum(int v, int l, int r, vector<int>& tr, int ql, int qr) {
-	if (l >= ql && r <= qr) // если мы нашли нужный отрезок
+void update(int p, int l, int r, int x, int val) {
+	if (x < l || x > r)
 	{
-		return tr[v];
-	}
-	if (ql >= r || qr <= l ) // если поиск вышел за пределы
-	{
-		return -1e9; // возвращаем любое значение не влияющие на результат; в RMQ это -1e9
-	}
-	int m = (l + r) / 2; // если все норм идем по левой и правой ветке
-	return get_sum(2 * v + 1, l, m, tr, ql, qr)+ get_sum(2 * v + 2, m, r, tr, ql, qr);
-}
-
-void update(int v, int l, int r, vector<int>& it, int qidx, int qvalue) {
-	if (r-l == 1)
-	{
-		it[v] = qvalue; // мы нашли зн.
 		return;
 	}
-	int m = (l + r) / 2;
-	if (qidx < m) //  мы ищем значение (m;m) если значение в отрезке левее значит идем влево
+	if (l == r)
 	{
-		update(2 * v + 1, l, m, it, qidx, qvalue);
-	}else {
-		update(2 * v + 2, m, r, it, qidx, qvalue);
+		tree[p] = val;
+		return;
 	}
-	it[v] = it[2 * v + 1] + it[2 * v + 2];
+	int mid = (l + r) / 2;
+	
+	update(p * 2 + 1, l, mid, x, val);
+	update(p * 2 + 2, mid + 1, r, x, val);
+	
+	tree[p] = tree[p * 2 + 1] + tree[p * 2 + 2];
+}
+ll getsum(int p, int l, int r, int ql, int qr) {
+	if (ql >r || qr < l)
+	{
+		return 0;
+	}
+	if (ql <= l &&  r<= qr )
+	{
+		return tree[p];
+	}
+	int mid = (l + r) / 2;
+	return getsum(2 * p + 1, l, mid, ql, qr) + getsum(2 * p + 2, mid + 1 , r, ql, qr);
+
+}
+int get(int p, int l, int r, int x) {
+
+	if (l == r)
+	{
+		return l;
+	}
+	int  mid = (l + r) / 2;
+	if (x <= tree[p * 2 + 1])
+	{
+		get(p * 2 + 1, l, mid, x);
+	}
+	else {
+		get(p * 2 + 2, mid + 1, r, x - tree[p * 2 + 1]);
+	}
 }
 int main() {
 	ios::sync_with_stdio(0);
