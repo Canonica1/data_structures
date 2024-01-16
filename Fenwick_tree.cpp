7adba2cbcf855287f705002287a2950c56e08ca3
Владислав Романовский <88035227+Canonica1@users.noreply.github.com>
2024-01-16 20:29:55 +0300

    Update Fenwick_tree.cpp

--- Fenwick_tree.cpp
+++ Fenwick_tree.cpp
@@ -1,26 +1,43 @@
-int sum(ve<int>& f, int r)
-{
-	int result = 0;
-	for (; r > 0; r -= r & -r)
-		result += f[r];
-	return result;
-}
-int sum(ve<int>& f, int l, int r) {
-	return sum(f, r) - sum(f, l - 1);
-}
-void inc(ve<int>& f, int i, int c) {
- 
-	while (i <= n)
-	{
-		f[i] += c;
-		i += i & -i;
-	}
-}
-void remove(ve<int>& f, int i, int c) {
- 
-	while (i <= n)
-	{
-		f[i] -= c;
-		i += i & -i;
-	}
-}
+struct Fenwick {
+    int n;
+    vector<int> a;
+
+    Fenwick(int n_ = 0) {
+        init(n_);
+    }
+
+    void init(int n_) {
+        n = n_;
+        a.assign(n, int{});
+    }
+
+    void add(int x, const int &v) {
+        for (int i = x + 1; i <= n; i += i & -i) {
+            a[i - 1] = a[i - 1] + v;
+        }
+    }
+
+    int sum(int x) {
+        int ans{};
+        for (int i = x; i > 0; i -= i & -i) {
+            ans = ans + a[i - 1];
+        }
+        return ans;
+    }
+
+    int rangeSum(int l, int r) {
+        return sum(r + 1) - sum(l);
+    }
+
+    int select(const int &k) {
+        int x = 0;
+        int cur{};
+        for (int i = 1 << __lg(n); i; i /= 2) {
+            if (x + i <= n && cur + a[x + i - 1] <= k) {
+                x += i;
+                cur = cur + a[x - 1];
+            }
+        }
+        return x;
+    }
+};
